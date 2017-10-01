4e85f9fae142313e83303343561d1706c75ada8f
Sam <Sam.zandee@gmail.com>
2017-10-01 17:27:17 +0200

    update 1okt

--- Week_3-1/main.cpp
+++ Week_3-1/main.cpp
@@ -18,74 +18,99 @@ constexpr hwlib::location square(const hwlib::location& lhs)
     return hwlib::location(m, m);
 }
 
-template< int N, typename T >
-class lookup_groot {
-private:    
-   T values[ N ];
-public: 
-   template< typename F >
-   constexpr lookup_groot( F function ){
-      for( int i = 0; i < N; ++i ){
-          values[ i ] = function( i );
-      }
-   }
-   constexpr T get( int n ) const {
-      return values[ n ];    
-   }
-};
-
-template< int N, typename T >
-class lookup_klein {
-private:    
-   T values[ N ];
-public: 
-   template< typename F >
-   constexpr lookup_klein( F function ){
-      for( int i = 0; i < N; ++i ){
-          values[ i ] = function( i );
-      }
-   }
-   constexpr T get( int n ) const {
-      return values[ n ];    
-   }
-};
-
-constexpr void klok_groot(auto &oled, auto middenpunt)
+template <int N, typename T> class lookup
 {
-    for(int angle_degrees = -180; angle_degrees < 180; angle_degrees += 6) {
-	hwlib::circle(hwlib::location(
-	(middenpunt.x + scaled_sine_from_degrees(angle_degrees, 30)),
-	(middenpunt.y + scaled_cosine_from_degrees(angle_degrees, 30))
-	),1).draw(oled);
+private:
+    T values[N];
+
+public:
+    template <typename F> constexpr lookup(F function)
+    {
+	for(int i = 0; i < N; ++i) {
+	    values[i] = function(i);
+	}
     }
+    constexpr T get(int n) const
+    {
+	return values[n];
+    }
+};
+constexpr auto circleklein_sin(int degrees){
+	return 32 + scaled_sine_from_degrees( degrees, 20);
 }
 
-constexpr void klok_klein(auto &oled, auto middenpunt)
-{
-    for(int angle_degrees = -180; angle_degrees < 180; angle_degrees += 30) {
-	hwlib::circle(hwlib::location(
-	(middenpunt.x + scaled_sine_from_degrees(angle_degrees, 20)),
-	(middenpunt.y + scaled_cosine_from_degrees(angle_degrees, 20))
-	),1).draw(oled);
-    }
+constexpr auto circleklein_cos(int degrees){
+	return 64 + scaled_cosine_from_degrees( degrees, 20);
 }
 
+constexpr auto circlegroot_sin(int degrees){
+	return 32 + scaled_sine_from_degrees( degrees, 30);
+}
+
+constexpr auto circlegroot_cos(int degrees){
+	return 64 + scaled_cosine_from_degrees( degrees, 30);
+}
+
+void tikken(auto &oled, auto middenpunt, 
+					auto cos_klein, auto cos_groot, auto sin_klein, auto sin_groot){
+	int uur = 0;
+	int minuut = 0;
+		
+	for(;;){
+		hwlib::wait_ms(200);
+		oled.clear();
+		minuut += 6;
+		if(minuut == 360){
+		minuut = 0;}
+		
+		hwlib::line( 
+		middenpunt, 
+		hwlib::location(
+		cos_klein.get(uur*30), 
+		sin_klein.get(uur*30))
+		).draw( oled );   
+
+		hwlib::line( 
+		middenpunt, 
+		hwlib::location(
+		cos_groot.get(minuut), 
+		sin_groot.get(minuut))
+	  ).draw( oled );
+	  }
+		oled.flush();
+}
 int main(void)
 {
-
     // kill the watchdog & wait for the PC console to start
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(500);
 
-    auto scl = target::pin_oc(target::pins::scl);
-    auto sda = target::pin_oc(target::pins::sda);
-    auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda(scl, sda);
-    auto oled = hwlib::glcd_oled_buffered(i2c_bus, 0x3c);
+    auto scl 		= target::pin_oc(target::pins::scl);
+    auto sda 		= target::pin_oc(target::pins::sda);
+    auto i2c_bus 	= hwlib::i2c_bus_bit_banged_scl_sda(scl, sda);
+    auto oled 		= hwlib::glcd_oled_buffered(i2c_bus, 0x3c);
     auto middenpunt = hwlib::location(64, 32);
+	auto sin_klein 	= lookup<360, int>(circleklein_sin);
+	auto cos_klein 	= lookup<360, int>(circleklein_cos);
+	auto sin_groot 	= lookup<360, int>(circlegroot_sin);
+	auto cos_groot 	= lookup<360, int>(circlegroot_cos);
 
     oled.clear();
     hwlib::circle(oled.size / 2, square(oled.size).x / 20).draw(oled);
-    klok_groot(oled, middenpunt);
-    klok_klein(oled, middenpunt);
+    oled.write(hwlib::location(64, 32));
+	
+    for(int uren = 0; uren < 360; uren += 6) {
+		oled.write(hwlib::location(
+		cos_groot.get(uren), 
+		sin_groot.get(uren))); 
+    }
+	
+	  for(int uren = 0; uren < 360; uren += 30) {
+		oled.write(hwlib::location(
+		cos_klein.get(uren), 
+		sin_klein.get(uren))); 
+	  }
+	tikken(oled, middenpunt, cos_klein, cos_groot, sin_klein, sin_groot);
     oled.flush();
 }
+	
--- Week_3-1/math_fun.hpp
+++ Week_3-1/math_fun.hpp
@@ -3,31 +3,31 @@
 #ifndef math_fun_hpp
 #define math_fun_hpp
 
-double pow( double g, int n ){
+constexpr double pow( double g, int n ){
    return ( n < 1 ) ? 1 : g * pow( g, n - 1 ); 
 }    
 
-double fac( int n ){ 
+constexpr double fac( int n ){ 
    return ( n < 2 ) ? 1 : n * fac( n - 1 ); 
 }
 
-double taylor( double a, int n ){
+constexpr double taylor( double a, int n ){
     return ( n < 0 ) ? 0 : pow( a, n ) / fac( n ) - taylor( a, n - 2 );
 }
 
-double sin( double a ){
+constexpr double sin( double a ){
    return taylor( a, 17 );
 }
 
-double radians_from_degrees( int degrees ){
+constexpr double radians_from_degrees( int degrees ){
    return 2 * 3.14 * ( degrees / 360.0 );
 }
 
-int scaled_sine_from_degrees( int degrees, int scale ){
+constexpr int scaled_sine_from_degrees( int degrees, int scale ){
    return scale * sin( radians_from_degrees( degrees ));
 }
 
-int scaled_cosine_from_degrees( int degrees, int scale ){
+constexpr int scaled_cosine_from_degrees( int degrees, int scale ){
    return scale * sin( radians_from_degrees( degrees - 90 ));
 }
 #endif
