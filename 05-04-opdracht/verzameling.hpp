ceade5b052a1f8cd2b878444b25df55c5bc914b9
Sam <Sam.zandee@gmail.com>
2023-01-23 10:38:10 +0100

    final

--- 05-04-opdracht/verzameling.hpp
+++ 05-04-opdracht/verzameling.hpp
@@ -1,89 +1,90 @@
 #ifndef VERZAMELING_HPP
 #define VERZAMELING_HPP
 
+#include <array>
 #include <iostream>
 
-template <int N, typename T> class verzameling {
-private:
-  std::array<T, N> set;
-  int filled = 0;
+template <int N, typename T>
+class verzameling {
+  private:
+   std::array<T, N> set;
+   int filled = 0;
 
-public:
-  verzameling() {}
-  void add(T item) {
-    for (int i = 0; i < filled; i++) {
-      if (set[i] == item) {
-        return;
+  public:
+   verzameling() {}
+   void add(T item) {
+      for (int i = 0; i < filled; i++) {
+         if (set[i] == item) {
+            return;
+         }
       }
-    }
-    set[filled] = item;
-    filled++;
-  }
-  void remove(T item) {
-    for (int i = 0; i < filled; i++) {
-      if (set[i] == item) {
-        for (int j = i; j < filled; j++) {
-          set[j] = set[j + 1];
-        }
-        filled--;
-        return;
+      set[filled] = item;
+      filled++;
+   }
+   void remove(T item) {
+      for (int i = 0; i < filled; i++) {
+         if (set[i] == item) {
+            for (int j = i; j < filled; j++) {
+               set[j] = set[j + 1];
+            }
+            filled--;
+            return;
+         }
       }
-    }
-  }
-  bool contains(T item) {
-    for (int i = 0; i < filled; i++) {
-      if (set[i] == item) {
-        return true;
+   }
+   bool contains(T item) {
+      for (int i = 0; i < filled; i++) {
+         if (set[i] == item) {
+            return true;
+         }
       }
-    }
-    return false;
-  }
-  T max() {
-    if (filled == 0) {
-      std::cout << "verzameling is leeg" << std::endl;
-      return set[0];
-    } else if (filled == 1) {
-      return set[0];
-    } else {
-      T max = set[0];
-      for (int check = 1; check < filled; check++) {
-        if (set[check] > max) {
-          max = set[check];
-        }
+      return false;
+   }
+   T max() {
+      if (filled == 0) {
+         std::cout << "verzameling is leeg" << std::endl;
+         return set[0];
+      } else if (filled == 1) {
+         return set[0];
+      } else {
+         T max = set[0];
+         for (int check = 1; check < filled; check++) {
+            if (set[check] > max) {
+               max = set[check];
+            }
+         }
+         return max;
       }
-      return max;
-    }
-  }
-  friend std::ostream &operator<<(std::ostream &os, const verzameling &rhs) {
+   }
+   friend std::ostream &operator<<(std::ostream &os, const verzameling &rhs) {
+      for (int i = 0; i < rhs.filled; i++) {
+         os << rhs.set[i];
+         if (i != rhs.filled - 1) {
+            os << ",";
+         }
+      }
 
-    for (int i = 0; i < rhs.filled; i++) {
-      os << rhs.set[i];
-      if (i != rhs.filled - 1) {
-        os << ",";
-      }
-    }
-
-    return os;
-  }
+      return os;
+   }
 };
 
 template <int N>
 bool operator>(std::array<char, N> &lhs, std::array<char, N> &rhs) {
-  char left = 0;
-  char right = 0;
-  for (char i : lhs) {
-    left += i;
-  }
-  for (char i : rhs) {
-    right += i;
-  }
-  return left > right;
+   char left = 0;
+   char right = 0;
+   for (char i : lhs) {
+      left += i;
+   }
+   for (char i : rhs) {
+      right += i;
+   }
+   return left > right;
 }
 
 std::ostream &operator<<(std::ostream &lhs, const std::array<char, 3> &rhs) {
-  for (int i = 0; i < 3; i++) {
-    lhs << rhs[i];
-  }
-  return lhs;
+   for (int i = 0; i < 3; i++) {
+      lhs << rhs[i];
+   }
+   return lhs;
 }
-#endif // VERZAMELING_HPP
+#endif  // VERZAMELING_HPP
